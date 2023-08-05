eb9ea6d3960cf769b74352199b3a0153a479afcc
mohamedeld <mohamed.azoz20010@gmail.com>
2023-08-05 18:39:05 +0300

    initial commit

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,24 @@
+
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem = ({expense}) =>  {
+    
+  return (
+     <>
+      <Card className="expense-item">
+        <div>
+            <ExpenseDate date={expense.date}/>
+        </div>
+        <div className="expense-item__description">
+            <h2>{expense.title}</h2>
+            <div className="expense-item__price">${expense.amount}</div>
+        </div>
+        
+        </Card>
+     </>
+  )
+}
+
+export default ExpenseItem
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,25 @@
+import Card from './Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = () => {
+    const expenses = [
+        {id:1,title:'Car Instance',amount:123.3,date:new Date(2023,2,25)},
+        {id:2,title:'Car Instance2',amount:225.3,date:new Date(2022,2,25)},
+        {id:3,title:'Car Instance3',amount:629.3,date:new Date(2021,6,15)}
+      ];
+  return (
+    <>
+        <Card className='expenses'>
+    {
+      expenses.map(expense=>(
+        <ExpenseItem key={expense.id} expense={expense}/>    
+      ))
+    }
+    
+    </Card>
+    </>
+  )
+}
+
+export default Expenses
